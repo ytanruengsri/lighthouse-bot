1ae4da4d88f8273b11985febb851165cb3ab5398
Yuthasak Tanruengsri <yuthasak.tanruengsri@zalando.de>
2018-04-08 14:18:26 +0200

    initial setup

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = robot => {
+  robot.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+}
